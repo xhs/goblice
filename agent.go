a9df63f02a723087510aa5b48669993447802f7e
xhs <chef@dark.kitchen>
2015-03-23 20:25:01 +0800

    run gofmt

--- agent.go
+++ agent.go
@@ -17,162 +17,162 @@ static void g_object_set_string_wrap(gpointer object, const char *property, cons
 import "C"
 
 import (
-  "errors"
-  "unsafe"
+	"errors"
+	"unsafe"
 )
 
 type Agent struct {
-  agent *C.NiceAgent
-  loop *C.GMainLoop
+	agent *C.NiceAgent
+	loop  *C.GMainLoop
 }
 
 type Candidate struct {
-  cand *C.NiceCandidate
+	cand *C.NiceCandidate
 }
 
 func NewAgent() (*Agent, error) {
-  var loop *C.GMainLoop
-  loop = C.g_main_loop_new(nil, 0)
-  if loop == nil {
-    return nil, errors.New("failed to create new main loop")
-  }
+	var loop *C.GMainLoop
+	loop = C.g_main_loop_new(nil, 0)
+	if loop == nil {
+		return nil, errors.New("failed to create new main loop")
+	}
 
-  var agent *C.NiceAgent
-  agent = C.nice_agent_new(C.g_main_loop_get_context(loop), C.NICE_COMPATIBILITY_RFC5245)
-  if agent == nil {
-    return nil, errors.New("failed to create new agent")
-  }
+	var agent *C.NiceAgent
+	agent = C.nice_agent_new(C.g_main_loop_get_context(loop), C.NICE_COMPATIBILITY_RFC5245)
+	if agent == nil {
+		return nil, errors.New("failed to create new agent")
+	}
 
-  return &Agent{agent, loop}, nil
+	return &Agent{agent, loop}, nil
 }
 
 func NewReliableAgent() (*Agent, error) {
-  var loop *C.GMainLoop
-  loop = C.g_main_loop_new(nil, 0)
-  if loop == nil {
-    return nil, errors.New("failed to create new main loop")
-  }
+	var loop *C.GMainLoop
+	loop = C.g_main_loop_new(nil, 0)
+	if loop == nil {
+		return nil, errors.New("failed to create new main loop")
+	}
 
-  var agent *C.NiceAgent
-  agent = C.nice_agent_new_reliable(C.g_main_loop_get_context(loop), C.NICE_COMPATIBILITY_RFC5245)
-  if agent == nil {
-    return nil, errors.New("failed to create new agent")
-  }
+	var agent *C.NiceAgent
+	agent = C.nice_agent_new_reliable(C.g_main_loop_get_context(loop), C.NICE_COMPATIBILITY_RFC5245)
+	if agent == nil {
+		return nil, errors.New("failed to create new agent")
+	}
 
-  return &Agent{agent, loop}, nil
+	return &Agent{agent, loop}, nil
 }
 
 func (a *Agent) Run() error {
-  C.g_main_loop_run(a.loop)
-  return nil
+	C.g_main_loop_run(a.loop)
+	return nil
 }
 
 func (a *Agent) Destroy() error {
-  C.g_main_loop_quit(a.loop)
-  C.g_object_unref(C.gpointer(a.agent))
-  C.g_main_loop_unref(a.loop)
-  return nil
+	C.g_main_loop_quit(a.loop)
+	C.g_object_unref(C.gpointer(a.agent))
+	C.g_main_loop_unref(a.loop)
+	return nil
 }
 
 func (a *Agent) SetControllingMode(controlling int) error {
-  s := C.CString("controlling-mode")
-  defer C.free(unsafe.Pointer(s))
-  C.g_object_set_int_wrap(C.gpointer(a.agent), s, C.int(controlling))
-  return nil
+	s := C.CString("controlling-mode")
+	defer C.free(unsafe.Pointer(s))
+	C.g_object_set_int_wrap(C.gpointer(a.agent), s, C.int(controlling))
+	return nil
 }
 
 func (a *Agent) SetStunServer(ip string) error {
-  s := C.CString("stun-server")
-  defer C.free(unsafe.Pointer(s))
-  v := C.CString(ip)
-  defer C.free(unsafe.Pointer(v))
-  C.g_object_set_string_wrap(C.gpointer(a.agent), s, v)
-  return nil
+	s := C.CString("stun-server")
+	defer C.free(unsafe.Pointer(s))
+	v := C.CString(ip)
+	defer C.free(unsafe.Pointer(v))
+	C.g_object_set_string_wrap(C.gpointer(a.agent), s, v)
+	return nil
 }
 
 func (a *Agent) SetStunServerPort(port int) error {
-  s := C.CString("stun-server-port")
-  defer C.free(unsafe.Pointer(s))
-  C.g_object_set_int_wrap(C.gpointer(a.agent), s, C.int(port))
-  return nil
+	s := C.CString("stun-server-port")
+	defer C.free(unsafe.Pointer(s))
+	C.g_object_set_int_wrap(C.gpointer(a.agent), s, C.int(port))
+	return nil
 }
 
 func (a *Agent) AddStream(components int) (int, error) {
-  rv := int(C.nice_agent_add_stream(a.agent, C.guint(components)))
-  if rv == 0 {
-    return 0, errors.New("failed to add stream")
-  }
-  return rv, nil
+	rv := int(C.nice_agent_add_stream(a.agent, C.guint(components)))
+	if rv == 0 {
+		return 0, errors.New("failed to add stream")
+	}
+	return rv, nil
 }
 
-func (a *Agent) SetStreamName(stream int, name string) (error) {
-  s := C.CString(name)
-  defer C.free(unsafe.Pointer(s))
-  rv := int(C.nice_agent_set_stream_name(a.agent, C.guint(stream), (*C.gchar)(s)))
-  if rv == 0 {
-    return errors.New("failed to set stream name")
-  }
-  return nil
+func (a *Agent) SetStreamName(stream int, name string) error {
+	s := C.CString(name)
+	defer C.free(unsafe.Pointer(s))
+	rv := int(C.nice_agent_set_stream_name(a.agent, C.guint(stream), (*C.gchar)(s)))
+	if rv == 0 {
+		return errors.New("failed to set stream name")
+	}
+	return nil
 }
 
 func (a *Agent) GatherCandidates(stream int) error {
-  rv := int(C.nice_agent_gather_candidates(a.agent, C.guint(stream)))
-  if rv == 0 {
-    return errors.New("failed to gather candidates")
-  }
-  return nil
+	rv := int(C.nice_agent_gather_candidates(a.agent, C.guint(stream)))
+	if rv == 0 {
+		return errors.New("failed to gather candidates")
+	}
+	return nil
 }
 
 func (a *Agent) Send(stream, component, length int, buf *[]byte) (int, error) {
-  tv := C.nice_agent_send(a.agent, C.guint(stream), C.guint(component),
-                          C.guint(length), (*C.gchar)(unsafe.Pointer(buf)))
-  if tv < 0 {
-    return 0, errors.New("failed to send data")
-  }
-  return int(tv), nil
+	tv := C.nice_agent_send(a.agent, C.guint(stream), C.guint(component),
+		C.guint(length), (*C.gchar)(unsafe.Pointer(buf)))
+	if tv < 0 {
+		return 0, errors.New("failed to send data")
+	}
+	return int(tv), nil
 }
 
 func (a *Agent) Receive(stream, component, length int, buf *[]byte) (int, error) {
-  rv := C.nice_agent_recv(a.agent, C.guint(stream), C.guint(component),
-                          (*C.guint8)(unsafe.Pointer(buf)), C.gsize(length), nil, nil)
-  if rv < 0 {
-    return 0, errors.New("failed to receive data")
-  } 
-  return int(rv), nil
+	rv := C.nice_agent_recv(a.agent, C.guint(stream), C.guint(component),
+		(*C.guint8)(unsafe.Pointer(buf)), C.gsize(length), nil, nil)
+	if rv < 0 {
+		return 0, errors.New("failed to receive data")
+	}
+	return int(rv), nil
 }
 
 func (a *Agent) GenerateSdp() (string, error) {
-  s := C.nice_agent_generate_local_sdp(a.agent)
-  defer C.free(unsafe.Pointer(s))
-  return C.GoString((*C.char)(s)), nil
+	s := C.nice_agent_generate_local_sdp(a.agent)
+	defer C.free(unsafe.Pointer(s))
+	return C.GoString((*C.char)(s)), nil
 }
 
 func (a *Agent) ParseSdp(sdp string) (int, error) {
-  s := C.CString(sdp)
-  defer C.free(unsafe.Pointer(s))
-  rv := C.nice_agent_parse_remote_sdp(a.agent, (*C.gchar)(s))
-  if rv < 0 {
-    return 0, errors.New("invalid remote sdp")
-  }
-  return int(rv), nil
+	s := C.CString(sdp)
+	defer C.free(unsafe.Pointer(s))
+	rv := C.nice_agent_parse_remote_sdp(a.agent, (*C.gchar)(s))
+	if rv < 0 {
+		return 0, errors.New("invalid remote sdp")
+	}
+	return int(rv), nil
 }
 
 func (a *Agent) ParseCandidateSdp(stream int, sdp string) (*Candidate, error) {
-  s := C.CString(sdp)
-  defer C.free(unsafe.Pointer(s))
-  c := C.nice_agent_parse_remote_candidate_sdp(a.agent, C.guint(stream), (*C.gchar)(s))
-  if c == nil {
-    return nil, errors.New("invalid remote candidate sdp")
-  }
-  return &Candidate{c}, nil
+	s := C.CString(sdp)
+	defer C.free(unsafe.Pointer(s))
+	c := C.nice_agent_parse_remote_candidate_sdp(a.agent, C.guint(stream), (*C.gchar)(s))
+	if c == nil {
+		return nil, errors.New("invalid remote candidate sdp")
+	}
+	return &Candidate{c}, nil
 }
 
 func (a *Agent) AddRemoteCandidate(stream int, c *Candidate) (int, error) {
-  list := C.g_slist_append(nil, C.gpointer(c.cand))
-  defer C.g_slist_free(list)
-  rv := C.nice_agent_set_remote_candidates(a.agent, C.guint(stream), 1, list)
-  if rv < 0 {
-    return 0, errors.New("failed to add remote candidate")
-  }
-  return int(rv), nil
+	list := C.g_slist_append(nil, C.gpointer(c.cand))
+	defer C.g_slist_free(list)
+	rv := C.nice_agent_set_remote_candidates(a.agent, C.guint(stream), 1, list)
+	if rv < 0 {
+		return 0, errors.New("failed to add remote candidate")
+	}
+	return int(rv), nil
 }
--- ugly.go
+++ ugly.go
@@ -10,7 +10,7 @@ static void set_candidate_gathering_done_cb(NiceAgent *agent, gpointer udata) {
                      G_CALLBACK(go_candidate_gathering_done_cb), udata);
 }
 
-extern void go_component_state_changed_cb(NiceAgent *agent, guint stream, 
+extern void go_component_state_changed_cb(NiceAgent *agent, guint stream,
                                           guint component, guint state, gpointer udata);
 static void set_component_state_changed_cb(NiceAgent *agent, gpointer udata) {
     g_signal_connect(G_OBJECT(agent), "component-state-changed",
@@ -33,74 +33,74 @@ static void set_new_selected_pair_cb(NiceAgent *agent, gpointer udata) {
 import "C"
 
 import (
-  "unsafe"
+	"unsafe"
 )
 
 type CallbackData struct {
-  cb interface{}
-  data interface{}
+	cb   interface{}
+	data interface{}
 }
 
 type CandidateGatheringDoneCB func(a *Agent, stream int, udata interface{})
 
 //export go_candidate_gathering_done_cb
 func go_candidate_gathering_done_cb(agent *C.NiceAgent, stream C.guint, udata unsafe.Pointer) {
-  d := (*CallbackData)(udata)
-  a := &Agent{agent: agent}
-  d.cb.(CandidateGatheringDoneCB)(a, int(stream), d.data)
+	d := (*CallbackData)(udata)
+	a := &Agent{agent: agent}
+	d.cb.(CandidateGatheringDoneCB)(a, int(stream), d.data)
 }
 
 func (a *Agent) SetCandidateGatheringDoneCB(cb CandidateGatheringDoneCB, udata interface{}) error {
-  d := unsafe.Pointer(&CallbackData{cb, udata})
-  C.set_candidate_gathering_done_cb(a.agent, C.gpointer(d))
-  return nil
+	d := unsafe.Pointer(&CallbackData{cb, udata})
+	C.set_candidate_gathering_done_cb(a.agent, C.gpointer(d))
+	return nil
 }
 
 type ComponentStateChangedCB func(a *Agent, stream, component, state int, udata interface{})
 
 //export go_component_state_changed_cb
 func go_component_state_changed_cb(agent *C.NiceAgent, stream, component, state C.guint, udata unsafe.Pointer) {
-  d := (*CallbackData)(udata)
-  a := &Agent{agent: agent}
-  d.cb.(ComponentStateChangedCB)(a, int(stream), int(component), int(state), d.data)
+	d := (*CallbackData)(udata)
+	a := &Agent{agent: agent}
+	d.cb.(ComponentStateChangedCB)(a, int(stream), int(component), int(state), d.data)
 }
 
 func (a *Agent) SetComponentStateChangedCB(cb ComponentStateChangedCB, udata interface{}) error {
-  d := unsafe.Pointer(&CallbackData{cb, udata})
-  C.set_component_state_changed_cb(a.agent, C.gpointer(d))
-  return nil
+	d := unsafe.Pointer(&CallbackData{cb, udata})
+	C.set_component_state_changed_cb(a.agent, C.gpointer(d))
+	return nil
 }
 
 type NewCandidateCB func(a *Agent, c *Candidate, udata interface{})
 
 //export go_new_candidate_cb
 func go_new_candidate_cb(agent *C.NiceAgent, candidate *C.NiceCandidate, udata unsafe.Pointer) {
-  d := (*CallbackData)(udata)
-  a := &Agent{agent: agent}
-  c := &Candidate{candidate}
-  d.cb.(NewCandidateCB)(a, c, d.data)
+	d := (*CallbackData)(udata)
+	a := &Agent{agent: agent}
+	c := &Candidate{candidate}
+	d.cb.(NewCandidateCB)(a, c, d.data)
 }
 
 func (a *Agent) SetNewCandidateCB(cb NewCandidateCB, udata interface{}) error {
-  d := unsafe.Pointer(&CallbackData{cb, udata})
-  C.set_new_candidate_cb(a.agent, C.gpointer(d))
-  return nil
+	d := unsafe.Pointer(&CallbackData{cb, udata})
+	C.set_new_candidate_cb(a.agent, C.gpointer(d))
+	return nil
 }
 
 type NewSelectedPairCB func(a *Agent, stream, component int, l, r *Candidate, udata interface{})
 
 //export go_new_selected_pair_cb
 func go_new_selected_pair_cb(agent *C.NiceAgent, stream, component C.guint,
-                             lcand, rcand *C.NiceCandidate, udata unsafe.Pointer) {
-  d := (*CallbackData)(udata)
-  a := &Agent{agent: agent}
-  l := &Candidate{lcand}
-  r := &Candidate{rcand}
-  d.cb.(NewSelectedPairCB)(a, int(stream), int(component), l, r, d.data)
+	lcand, rcand *C.NiceCandidate, udata unsafe.Pointer) {
+	d := (*CallbackData)(udata)
+	a := &Agent{agent: agent}
+	l := &Candidate{lcand}
+	r := &Candidate{rcand}
+	d.cb.(NewSelectedPairCB)(a, int(stream), int(component), l, r, d.data)
 }
 
 func (a *Agent) SetNewSelectedPairCB(cb NewSelectedPairCB, udata interface{}) error {
-  d := unsafe.Pointer(&CallbackData{cb, udata})
-  C.set_new_selected_pair_cb(a.agent, C.gpointer(d))
-  return nil
+	d := unsafe.Pointer(&CallbackData{cb, udata})
+	C.set_new_selected_pair_cb(a.agent, C.gpointer(d))
+	return nil
 }
