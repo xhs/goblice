180c0f9d2fe7162f70601aa4041899ccf7a1a31c
xhs <chef@dark.kitchen>
2015-04-02 03:47:42 +0800

    add more tests

--- agent_test.go
+++ agent_test.go
@@ -50,3 +50,78 @@ func TestGenerateCandidates(t *testing.T) {
     break
   }
 }
+
+func TestGenerateOffer(t *testing.T) {
+  agent, _ := goblice.NewAgent()
+  defer agent.Destroy()
+
+  log.Print(agent.GenerateSdp())
+}
+
+func TestIceNegotiation(t *testing.T) {
+  client, _ := goblice.NewAgent()
+  if err := client.GatherCandidates(); err != nil {
+    t.Error(err)
+  }
+
+  server, _ := goblice.NewAgent()
+  if err := server.GatherCandidates(); err != nil {
+    t.Error(err)
+  }
+
+  server.ParseSdp(client.GenerateSdp())
+  client.ParseSdp(server.GenerateSdp())
+
+  go client.Run()
+  go server.Run()
+
+  clientTimeout := time.After(2 * time.Second)
+
+  go func () {
+    for {
+      select {
+      case cand := <-client.Candidates:
+        log.Print("client candidate:", cand)
+        server.ParseCandidateSdp(cand)
+        continue
+      case e := <-client.Events:
+        if e == goblice.EventNegotiationDone {
+          log.Print("client negotiation done")
+          client.Send([]byte("hello"))
+        }
+        continue
+      case <-clientTimeout:
+        log.Print("client timeout")
+        client.Destroy()
+        break
+      }
+
+      break
+    }
+  }()
+
+  serverTimeout := time.After(2 * time.Second)
+
+  for {
+    select {
+    case cand := <-server.Candidates:
+      log.Print("server candidate:", cand)
+      server.ParseCandidateSdp(cand)
+      continue
+    case e := <-server.Events:
+      if e == goblice.EventNegotiationDone {
+        log.Print("server negotiation done")
+      }
+      continue
+    case d := <-server.DataToRead:
+      log.Print("server received:", d)
+      continue
+    case <-serverTimeout:
+      log.Print("server timeout")
+      server.Destroy()
+      break
+    }
+
+    break
+  }
+}
